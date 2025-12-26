5e8eac167c9e65dbd3d2b77b026e6ce20c3bc5ed
mmcgough8 <mwm9490@icloud.com>
2025-12-26 11:32:49 -0500

    "Created mvn project"

---
+++ questkeeper/src/main/java/com/questkeeper/Main.java
@@ -0,0 +1,24 @@
+package com.questkeeper;
+
+/**
+ * QuestKeeper - A CLI-based D&D 5e Auto DM Adventure Game
+ * 
+ * Entry point for the application.
+ * 
+ * @author Marc McGough
+ * @version 1.0.0
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("=".repeat(50));
+        System.out.println("       ⚔ QUESTKEEPER ⚔");
+        System.out.println("   A D&D 5e Auto DM Adventure");
+        System.out.println("=".repeat(50));
+        System.out.println();
+        System.out.println("Welcome, adventurer!");
+        System.out.println("Project setup successful.");
+        System.out.println();
+        System.out.println("Next: Implement character creation...");
+    }
+}
