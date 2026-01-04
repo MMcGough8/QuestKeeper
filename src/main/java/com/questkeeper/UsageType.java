a0ccbf3efb22192898da58e6fb33aef07e76386c
mmcgough8 <mwm9490@icloud.com>
2026-01-04 15:34:13 -0500

    "Added UsageType enum class"

---
+++ src/main/java/com/questkeeper/UsageType.java
@@ -0,0 +1,56 @@
+package com.questkeeper;
+
+/**
+ * Defines how often an item effect can be used.
+ * 
+ * @author Marc McGough
+ * @version 1.0
+ */
+public enum UsageType {
+    
+    /**
+     * Can be used once per day, resets at dawn.
+     */
+    DAILY("Daily", "Resets at dawn"),
+    
+    /**
+     * Can be used once per long rest.
+     */
+    LONG_REST("Long Rest", "Resets after a long rest"),
+    
+    /**
+     * Has a limited number of charges that may or may not recharge.
+     */
+    CHARGES("Charges", "Limited charges"),
+    
+    /**
+     * Single use, consumed when used.
+     */
+    CONSUMABLE("Consumable", "Destroyed when used"),
+    
+    /**
+     * Can be used unlimited times with no restrictions.
+     */
+    UNLIMITED("Unlimited", "No usage limit"),
+    
+    /**
+     * Passive effect, always active when equipped.
+     */
+    PASSIVE("Passive", "Always active");
+    
+    private final String displayName;
+    private final String description;
+    
+    UsageType(String displayName, String description) {
+        this.displayName = displayName;
+        this.description = description;
+    }
+    
+    public String getDisplayName() {
+        return displayName;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+}
